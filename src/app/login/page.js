0e62195efa4a1771dbc21a96efbfa57ae9a3fef8
sougata <sougataghar47@gmail.com>
2025-09-08 20:15:16 +0530

    edited login file

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,31 +1,50 @@
 'use client';
 import { signIn } from "next-auth/react";
-import { redirect } from "next/navigation";
- 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
-export default  function Login(){
-    let [email,setEmail]=useState("");
-    let [password,setPassword]=useState("");
- 
-    let handleSign=async function(e){
-        e.preventDefault();
-        const result =await signIn("credentials", {
-            redirect: false,
-            email,
-            password,
-          });
-        if (result?.error) {
+
+export default function Login() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSign = async (e) => {
+    e.preventDefault();
+
+    const result = await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
+
+    if (result?.error) {
       alert("Invalid email or password");
-    } 
-        else {
-      redirect("/dashboard");
+    } else {
+      router.push("/dashboard"); 
     }
- 
-    }
-    return <form className="mt-[25%] p-4" onSubmit={handleSign}>
-        <h1 className="mb-8 font-bold">Sign In</h1>
-        <input className="mb-8 border-2 border-sky-500 w-full py-4 px-2 placeholder:italic" onInput={e => setEmail(e.target.value)} placeholder="Email" type="email"/>
-        <input className="mb-8 border-2 border-sky-500 w-full py-4 px-2 placeholder:italic" onInput={e => setPassword(e.target.value)} placeholder="Password" type="password"/>
-        <button className="block rounded-full border-0 bg-sky-500 text-white px-4 py-2 text-sm font-semibold disabled:opacity-50" type="submit">Submit</button>
+  };
+
+  return (
+    <form className="mt-[25%] p-4" onSubmit={handleSign}>
+      <h1 className="mb-8 font-bold">Sign In</h1>
+      <input
+        className="mb-8 border-2 border-sky-500 w-full py-4 px-2 placeholder:italic"
+        onInput={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+        type="email"
+      />
+      <input
+        className="mb-8 border-2 border-sky-500 w-full py-4 px-2 placeholder:italic"
+        onInput={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        type="password"
+      />
+      <button
+        className="block rounded-full border-0 bg-sky-500 text-white px-4 py-2 text-sm font-semibold disabled:opacity-50"
+        type="submit"
+      >
+        Submit
+      </button>
     </form>
+  );
 }
