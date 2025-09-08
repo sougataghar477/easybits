d6a81f6bfc981d8cefeaa15bc1b87e9f7824b290
sougata <sougataghar47@gmail.com>
2025-09-08 21:18:40 +0530

    added middleware

---
+++ src/middleware.js
@@ -0,0 +1,15 @@
+// export { default } from "next-auth/middleware"
+
+// export const config = { matcher: ["/dashboard(.*)"] }
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login", // redirect here if no session
+  },
+});
+
+// Protect only dashboard routes
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
