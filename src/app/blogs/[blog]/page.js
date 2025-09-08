89560cec39b23fbb7df650f342244efca4a84cf8
sougata <sougataghar47@gmail.com>
2025-09-08 19:05:50 +0530

    force dynamic

--- src/app/blogs/[blog]/page.js
+++ src/app/blogs/[blog]/page.js
@@ -2,28 +2,27 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 
+export const dynamic = "force-dynamic"; // ✅ prevents build error
+
 export default function Blog() {
   const [blogDetails, setBlogDetails] = useState(null);
   const [loading, setLoading] = useState(true);
   const { blog } = useParams();
-useEffect(() => {
-  setLoading(true);
-  fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs/${blog}`)
-    .then(async (r) => { return r.json();
-    })
-    .then((d) => setBlogDetails(d))
-    .catch((err) => {
-      setBlogDetails(null);
-    })
-    .finally(() => setLoading(false));
-}, [blog]);
 
+  useEffect(() => {
+    setLoading(true);
+    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs/${blog}`)
+      .then((r) => r.json())
+      .then((d) => setBlogDetails(d))
+      .catch(() => setBlogDetails(null))
+      .finally(() => setLoading(false));
+  }, [blog]);
 
   if (loading) {
     return <p className="mt-4 text-gray-500">Loading blog...</p>;
   }
-  if(!loading && !blogDetails){
-    return <h1>Invalid Blog ID</h1>
+  if (!loading && !blogDetails) {
+    return <h1>Invalid Blog ID</h1>;
   }
   return (
     <>
@@ -32,9 +31,15 @@ useEffect(() => {
         by <span className="italic">{blogDetails.author}</span>
       </p>
       <p>
-        on <span className="font-bold italic">{new Date(blogDetails.date).toLocaleDateString()}</span>
+        on{" "}
+        <span className="font-bold italic">
+          {new Date(blogDetails.date).toLocaleDateString()}
+        </span>
       </p>
-      <img src="https://images.unsplash.com/photo-1496449903678-68ddcb189a24?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+      <img
+        src="https://images.unsplash.com/photo-1496449903678-68ddcb189a24?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="blog image"
+      />
       <p className="text-xl">{blogDetails.content}</p>
     </>
   );
