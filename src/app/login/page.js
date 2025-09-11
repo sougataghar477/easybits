9ff07d33e81d06203c107c2cc365d400c8338b7e
sougata <sougataghar47@gmail.com>
2025-09-11 22:52:18 +0530

    debugging /login in production

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -13,7 +13,7 @@ const handleSign = async (e) => {
 
   try {
     const result = await signIn("credentials", {
-      redirect: false,
+      redirect: '/dashboard',
       email,
       password,
     });
